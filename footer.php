a9b16a751711f0ba1642b8a6461218f8aedd4612
Marinko23 <85893400+Marinko23@users.noreply.github.com>
2023-03-18 12:40:25 +0100

    Rename footer.php to PHP files/footer.php

--- footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    © 2022 Milan Marinković i Filip Jovanović. Sva prava zadržana
-</footer>
-<!-- bootstrap js bundle https://getbootstrap.com/docs/5.2/getting-started/introduction/ -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
